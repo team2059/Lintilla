404a15d13df494ac95397d8d205a061e08468028
Ryan Fortner <ryankfortner@gmail.com>
2026-02-11 09:14:25 -0500

    Add sysid methods

--- src/main/java/org/team2059/Lintilla/subsystems/collector/Collector.java
+++ src/main/java/org/team2059/Lintilla/subsystems/collector/Collector.java
@@ -4,18 +4,28 @@
 
 package org.team2059.Lintilla.subsystems.collector;
 
+import edu.wpi.first.units.measure.MutAngle;
+import edu.wpi.first.units.measure.MutAngularVelocity;
+import edu.wpi.first.units.measure.MutVoltage;
+import edu.wpi.first.units.measure.Time;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 import org.littletonrobotics.junction.Logger;
 import org.team2059.Lintilla.Constants.CollectorConstants;
 
-import static edu.wpi.first.units.Units.Rotations;
+import static edu.wpi.first.units.Units.*;
 
 public class Collector extends SubsystemBase {
 
 	public CollectorIO io;
 	public CollectorIOInputsAutoLogged inputs;
+
+	private final SysIdRoutine routine;
+	private final MutVoltage appliedVoltageRoutine = Volts.mutable(0);
+	private final MutAngle angleRoutine = Rotations.mutable(0);
+	private final MutAngularVelocity angularVelocityRoutine = RPM.mutable(0);
 
 	/**
 	 * Creates a new Collector.
@@ -23,6 +33,27 @@ public class Collector extends SubsystemBase {
 	public Collector(CollectorIO io) {
 		this.io = io;
 		inputs = new CollectorIOInputsAutoLogged();
+
+		routine = new SysIdRoutine(
+		  new SysIdRoutine.Config(
+			Volts.of(0.5).per(Second), // Ramp rate in volts per second
+		    Volts.of(2), // Dynamic step voltage
+		    Time.ofBaseUnits(4, Second), // Test duration in seconds
+		    null
+		  ),
+		  new SysIdRoutine.Mechanism(
+			voltage -> {
+				io.setTiltVolts(voltage.in(Volts));
+			},
+		    log -> {
+				log.motor("collector-tiltmotor")
+				  .voltage(appliedVoltageRoutine.mut_replace(inputs.tiltAppliedVolts))
+				  .angularPosition(angleRoutine.mut_replace(inputs.tiltPosition))
+				  .angularVelocity(angularVelocityRoutine.mut_replace(inputs.tiltVelocity));
+		    },
+		    this
+		  )
+		);
 	}
 
 	/**
@@ -41,6 +72,22 @@ public class Collector extends SubsystemBase {
 		return this.runOnce(() -> io.setTiltPosition(CollectorConstants.thruBoreIn))
 		  .andThen(Commands.waitUntil(() -> inputs.tiltPosition.isNear(Rotations.of(CollectorConstants.thruBoreIn), 0.05)))
 		  .andThen(this.runOnce(() -> io.stopTilt()));
+	}
+
+	public Command sysIdQuasiForward() {
+		return routine.quasistatic(SysIdRoutine.Direction.kForward);
+	}
+
+	public Command sysIdQuasiReverse() {
+		return routine.quasistatic(SysIdRoutine.Direction.kReverse);
+	}
+
+	public Command sysIdDynamicForward() {
+		return routine.dynamic(SysIdRoutine.Direction.kForward);
+	}
+
+	public Command sysIdDynamicReverse() {
+		return routine.dynamic(SysIdRoutine.Direction.kReverse);
 	}
 
 	@Override
